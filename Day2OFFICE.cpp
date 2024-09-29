a18959739e99dcd7699fd2707c729a5f89e50af5
codexrgb <ps3312806@gmail.com>
2024-09-29 18:26:41 +0530

    Add files via upload

---
+++ Day2OFFICE.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+int main() {
+    int T;
+    std::cin >> T;
+
+    while (T--) {
+        int X, Y; // Hours from Monday to Thursday, hours on Friday
+        std::cin >> X >> Y;
+
+        // Calculate total hours
+        int totalHours = 4 * X + Y; // 4 days * X + Friday
+
+        std::cout << totalHours << std::endl;
+    }
+
+    return 0;
+}
+
